c696dc252ac4379a60ab132c86d38fd1c4cca200
Seop-kor <dkdn1004@naver.com>
2021-06-01 15:42:32 +0900

    06/01 media 쿼리 작성 완료 데이터베이스만 연동 시키면 완성

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
+// Contents Text Slice
 const detailText = document.querySelectorAll(".detail-text p a");
 detailText.forEach((item) => {
   if(item.textContent.length > 20){
     item.textContent = item.textContent.slice(0, 20) + "...";
   }
+});
+
+// Mobile Menu Rotate
+const mobileMenu = document.querySelector(".mobile-menu");
+mobileMenu.addEventListener('click', () => {
+  mobileMenu.classList.toggle("active");
 });
