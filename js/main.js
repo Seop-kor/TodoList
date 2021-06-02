6221ff0731770adcef6dc402ea66a4594e868d64
Seop-kor <dkdn1004@naver.com>
2021-06-02 18:25:20 +0900

    0602 Graph추가 완료

--- js/main.js
+++ js/main.js
@@ -11,3 +11,39 @@ const mobileMenu = document.querySelector(".mobile-menu");
 mobileMenu.addEventListener('click', () => {
   mobileMenu.classList.toggle("active");
 });
+
+/* each-graph Function */
+// let chart = window.chart = new EasyPieChart(document.querySelector('.chart'), {
+//   easing: 'easeOutElastic',
+//   delay: 3000,
+//   barColor: '#69c',
+//   trackColor: '#ace',
+//   scaleColor: false,
+//   lineWidth: 13,
+//   trackWidth: 10,
+//   lineCap: 'butt',
+//   onStep: function(from, to, percent) {
+//     this.el.children[0].innerHTML = Math.round(percent);
+//   }
+// });
+function makeChart(sel, barColor, trackColor){
+  return new EasyPieChart(document.querySelector(sel + " .chart"), {
+    easing: 'easeOutElastic',
+    delay: 3000,
+    barColor: barColor,
+    trackColor: trackColor,
+    scaleColor: false,
+    lineWidth: 10,
+    trackWidth: 8,
+    lineCap: 'round',
+    size: 180,
+    onStep: function(from, to, percent) {
+      this.el.children[0].innerHTML = Math.round(percent);
+    }
+  }); 
+}
+
+makeChart(".db", "#7c41f5", "#d4c4f5");
+makeChart(".api", "#ff9062", "#ffdbcc");
+makeChart(".renewal", "#3acbe8", "#bae0e8");
+makeChart(".planning", "#f541da", "#f0c4f5");
